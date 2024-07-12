6547bbadaeb7a25867bb2138e8e64db45a6630bf
ArduFish123 <80416599+ArduFish123@users.noreply.github.com>
2024-07-12 12:11:48 +0300

    Update ModelPredicateHelper.java

--- src/main/java/net/fabric_extras/ranged_weapon/client/ModelPredicateHelper.java
+++ src/main/java/net/fabric_extras/ranged_weapon/client/ModelPredicateHelper.java
@@ -10,24 +10,24 @@ import net.minecraft.util.Identifier;
 public class ModelPredicateHelper {
     public static void registerBowModelPredicates(CustomBow bow) {
         // We cannot reuse what is already registered for Vanilla bow, because it uses hardcoded pull time values
-        ModelPredicateProviderRegistry.register(bow, new Identifier("pull"), (stack, world, entity, seed) -> {
+        ModelPredicateProviderRegistry.register(bow, Identifier.of("pull"), (stack, world, entity, seed) -> {
             if (entity == null) {
                 return 0.0F;
             } else {
                 return entity.getActiveItem() != stack ? 0.0F : (float)(stack.getMaxUseTime() - entity.getItemUseTimeLeft()) / ((float) ((CustomRangedWeapon)bow).getRangedWeaponConfig().pull_time());
             }
         });
-        ModelPredicateProviderRegistry.register(bow, new Identifier("pulling"), (stack, world, entity, seed) -> {
+        ModelPredicateProviderRegistry.register(bow, Identifier.of("pulling"), (stack, world, entity, seed) -> {
             return entity != null && entity.isUsingItem() && entity.getActiveItem() == stack ? 1.0F : 0.0F;
         });
     }
 
     public static void registerCrossbowModelPredicates(CustomCrossbow crossbow) {
-        var predicatesToCopy = new Identifier[] {
-                new Identifier("pull"),
-                new Identifier("pulling"),
-                new Identifier("charged"),
-                new Identifier("firework")
+        var predicatesToCopy = Identifier.of[] {
+                Identifier.of("pull"),
+                Identifier.of("pulling"),
+                Identifier.of("charged"),
+                Identifier.of("firework")
         };
         for (var predicateId : predicatesToCopy) {
             var predicateProvider = ModelPredicateProviderRegistry.get(Items.CROSSBOW, predicateId);
